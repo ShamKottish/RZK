5c1b107bb5ec7c86e8cf053ac6575ec69fcc54d2
ShamKottish <shamq12@gmail.com>
2025-07-26 21:54:28 +0300

    Update index.tsx

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -4,7 +4,7 @@ import { Platform, StyleSheet } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
+export default function Home() {
   return (
     <>
       <ThemedView style={styles.titleContainer}>
